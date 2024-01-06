995d0c3da84f2ac2027a8ee700d559bdce45ee50
Ben Whitmore <32911992+benwh1@users.noreply.github.com>
2024-01-06 21:42:17 +0000

    Change default code

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,9 @@
-mod utils;
-
 use wasm_bindgen::prelude::*;
 
-#[wasm_bindgen]
-extern "C" {
-    fn alert(s: &str);
-}
+#[wasm_bindgen(start)]
+pub fn main() {
+    #[cfg(feature = "console_error_panic_hook")]
+    console_error_panic_hook::set_once();
 
-#[wasm_bindgen]
-pub fn greet() {
-    alert("Hello, {{project-name}}!");
+    log::info!("Hello, {{project-name}}!");
 }
