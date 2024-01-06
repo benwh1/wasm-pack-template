a887529e33d84e8d3cc151d38b2099d0196a5da5
Ben Whitmore <32911992+benwh1@users.noreply.github.com>
2024-01-06 23:37:27 +0000

    Add `wasm-logger`

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,7 @@ use wasm_bindgen::prelude::*;
 pub fn main() {
     #[cfg(feature = "console_error_panic_hook")]
     console_error_panic_hook::set_once();
+    wasm_logger::init(wasm_logger::Config::default());
 
     log::info!("Hello, {{project-name}}!");
 }
